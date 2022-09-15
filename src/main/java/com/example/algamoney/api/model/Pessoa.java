fabe2105f86c3efddd6a8460d6079e993c5068c0
Arthur Lisboa <lisboa.arthur159@gmail.com>
2022-09-15 20:26:05 -0300

    6.3

---
+++ src/main/java/com/example/algamoney/api/model/Pessoa.java
@@ -0,0 +1,91 @@
+package com.example.algamoney.api.model;
+
+import java.beans.Transient;
+import java.util.Objects;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@Embedded
+	private Endereco endereco;
+	
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	private Boolean ativo;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	@JsonIgnore
+	@javax.persistence.Transient
+	public boolean isInativo() {
+		return !this.ativo;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(codigo, other.codigo);
+	}
+	
+	
+	
+}
